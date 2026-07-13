802fc2435172bb5fce444ece3e46566dcde3dc41
Huzaifa <hf964691@gmail.com>
2026-07-13 00:48:59 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553;
+let accountEmail = "huzaifa@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 2; // not allowed
+
+accountEmail = "hd@gc.com";
+accountPassword = "215468";
+accountCity = "Chandigarh";
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
